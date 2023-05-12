34b2a6529c38dc732c55c7964483ce6886759d9a
Allen Russell <garyrusselljr@gmail.com>
2023-05-11 18:31:31 -0600

    Simplify append method

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -6,15 +6,7 @@ class LinkedList
   end
 
   def append(data)
-    if @head == nil
       @head = Node.new(data)
-    else
-      current_node = @head
-      while current_node.next_node != nil
-        current_node = current_node.next_node
-      end
-      current_node.next_node = Node.new(data)
-    end
   end
   
 end
