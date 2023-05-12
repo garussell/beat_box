264075ef99f3f053b5a318291bf8ec8d2c6e787a
Allen Russell <garyrusselljr@gmail.com>
2023-05-12 14:41:39 -0600

    Add test for prepend and insert methods

--- spec/linked_list_spec.rb
+++ spec/linked_list_spec.rb
@@ -19,7 +19,7 @@ describe "#head" do
 end
 
 describe "#append" do
-  it 'can add sounds to the list' do
+  it 'can add sounds to the end of list' do
     list = LinkedList.new
 
     expect(list.append("doop").data).to eq("doop")
@@ -60,6 +60,33 @@ describe "#multiple nodes" do
     expect(list.head.next_node.next_node).to eq(nil)
     expect(list.count).to eq(2)
     expect(list.to_string).to eq("doop deep")
+  end
+end
+
+describe "#prepend" do
+  it 'can add sounds to beginning of list' do
+    list = LinkedList.new
+
+    list.append("plop")
+    expect(list.to_string).to eq("plop")
+
+    expect(list.append("suu").data).to eq("suu")
+    expect(list.prepend("dop").data).to eq("dop")
+    expect(list.to_string).to eq("dop plop suu")
+    expect(list.count).to eq(3)
+  end
+end
+
+describe "#insert" do
+  it 'can add sounds at specified index' do
+    list = LinkedList.new
+
+    list.append("plop")
+    list.append("suu")
+    list.prepend("dop")
+
+    expect(list.insert(1, "woo")).to eq("woo")
+    expect(list.to_string).to eq ("dop woo plop suu")
     end
   end
 end 
