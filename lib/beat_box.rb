b97bbb2fba250bf01850e16277ef11c7bb91cd20
Allen Russell <garyrusselljr@gmail.com>
2023-05-14 16:47:00 -0600

    Add play method with Konnakol

--- lib/beat_box.rb
+++ lib/beat_box.rb
@@ -3,6 +3,19 @@ class BeatBox
 
   def initialize
     @list = LinkedList.new
-    # require 'pry';binding.pry
+  end
+
+  def append(data_string)
+    sound_bites_arr = data_string.split(" ")
+    sound_bites_arr.each { |sound| @list.append(sound) }
+    sound_bites_arr.join(" ")
+  end
+
+  def count
+    @list.count
+  end
+
+  def play(voice = "Samantha", speed = "225", beats = "Ki ta ta ke ta ka ju no ta ka di me ta ... ka ... di ... meh ... ta ka di ... meh ... ta ka di ... meh ta ka di me te ta ka di me te ta ka di me te Ki")
+    `say -r #{speed} -v #{voice} #{beats}`
   end
 end
