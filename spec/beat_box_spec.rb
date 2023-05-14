d927172364727260513422d34020bf00646f02d2
Allen Russell <garyrusselljr@gmail.com>
2023-05-14 16:26:48 -0600

    Add test for play method

--- spec/beat_box_spec.rb
+++ spec/beat_box_spec.rb
@@ -1,6 +1,6 @@
-require "./lib/beat_box"
-require "./lib/linked_list"
-require "./lib/node"
+require './lib/beat_box'
+require './lib/linked_list'
+require './lib/node'
 
 RSpec.describe BeatBox do
   describe "#exists" do
@@ -16,8 +16,24 @@ RSpec.describe BeatBox do
   describe "#append" do
     it 'can add a list of items' do
       bb = BeatBox.new
+      list = LinkedList.new
 
       expect(bb.append("deep doo ditt")).to eq("deep doo ditt")
+      
+      bb.append("woo hoo shu")
+
+      expect(bb.count).to eq(6)
+    end
+  end
+
+  describe "#play" do
+    it 'can say words' do
+      bb = BeatBox.new
+      list = LinkedList.new
+
+      bb.append("woo hoo shu")
+      
+      expect(bb.play).to eq(`say -r 500 -v Zarvox "woo hoo shu"`)
     end
   end
 end
