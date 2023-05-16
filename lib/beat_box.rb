5379df455c8b9aa4f8061731618246a6f08340d3
Allen Russell <garyrusselljr@gmail.com>
2023-05-15 19:16:10 -0600

    feat: Adds working all method

--- lib/beat_box.rb
+++ lib/beat_box.rb
@@ -21,32 +21,15 @@ class BeatBox
     @list.count
   end
 
-  def play(voice = "Samantha", speed = "225", beats = "doop da doop de doop")
-    `say -r #{speed} -v #{voice} #{beats}`
+  def play(voice = "Samantha", rate = "225", beats = "doop da doop de doop")
+    `say -r #{rate} -v #{voice} #{beats}`
   end
 
   def all
     sound_bites = @list.to_string.split
-    sound_bites.reject! { |word| word.split("-").count > 2 }       
+    sound_bites.reject! { |word| word.length > 4 }
+    sound_bites.join(" ")
   end
 
 end
 
-  # def syllable_counter
-  #   syllable_count = []
-  #   # vowels = %w[ a e i o u ]
-  #   @list.split("").each do |word|
-  #     if word.to_s.downcase.include?("a")
-  #       syllable_count << word
-  #     elsif word.to_s.downcase.include?("i")
-  #       syllable_count << word
-  #     elsif word.to_s.downcase.include?("o")
-  #       syllable_count << word
-  #     elsif word.to_s.downcase.include?("u")
-  #       syllable_count << word
-  #     elsif word.to_s.downcase.include?("y")
-  #       syllable_count << word
-  #     end
-  #   end
-  #   syllable_count.count
-  # end
